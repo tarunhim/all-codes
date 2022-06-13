1b333b181f61643e577cde68977cc37c15566466
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-06-13 10:47:55 +0530

    again

--- java codes/BinaryTree.java
+++ java codes/BinaryTree.java
@@ -1,12 +1,6 @@
-/******************************************************************************
 
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
 
-*******************************************************************************/
-
-class Main
+class BinaryTree
 {
 	public static void main(String[] args) {
 		Bst bst = new Bst();
---
+++ java codes/Practice.java
@@ -0,0 +1,5 @@
+class Practice{
+    public static void main(String[] args) {
+        System.out.print("My name is tarun kumar chourasiya");
+    }
+}
