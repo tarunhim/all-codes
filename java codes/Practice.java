9c121c81e2d1289250434031840f308265805200
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-07-07 10:39:33 +0530

    practice updates

--- java codes/Practice.java
+++ java codes/Practice.java
@@ -1,56 +1,42 @@
-import java.io.*; // for handling input/output
-import java.util.*; // contains Collections framework
-
-// don't change the name of this class
-// you can add inner classes if needed
-class Main {
-	static int min = 1000000000;
-    public static void main (String[] args) throws IOException {
-		String beginWord = "hit", endWord = "cog";
-		String[] wordList = {"hot","dot","dog","lot","log","cog"};
-		List<String> list = new ArrayList<>();
-		for(String i : wordList) list.add(i);
-		System.out.print(new Solution().findLadders(beginWord, endWord, list));
-	}
-}
-class Solution {
-    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        List<List<String>> list = new ArrayList<>();
-        Set<String> set = new HashSet<>();
-        set.addAll(wordList);
-        List<String> curr = new ArrayList<>();
-        curr.add(beginWord);
-        Queue<List<String>> q = new LinkedList<>();
-        q.offer(curr);
-        while(!q.isEmpty()) {
-            int size = q.size();
-            List<String> dlist = new ArrayList<>();
-            for(int k = 0; k < size; ++k) {
-            List<String> templ = q.poll();
-            String temp = templ.get(templ.size()-1);
-            if(temp.equals(endWord)) {
-                list.add(templ);
-                continue;
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        int[] arr = {6, 2, 5, 4, 5, 1, 6};
+        int n = arr.length;
+        int max = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for(int i = 0; i < n; ++i) {
+            while(stack.peek() != -1 && arr[i] <= arr[stack.peek()]) {
+                int temp = stack.pop();
+                max = Math.max(max,arr[temp]*(i-stack.peek()-1));
+                
             }
-           
-            
-            for(int i = 0; i < temp.length(); ++i) {
-				StringBuilder sb = new StringBuilder(temp);
-                for(char j = 'a'; j <= 'z'; ++j) {
-                    sb.setCharAt(i,j);
-                    if(temp.equals(sb.toString())) continue;
-                    if(set.contains(sb.toString())) {
-                        templ.add(sb.toString());
-                        q.add(new ArrayList<String>(templ));
-                        templ.remove(templ.size()-1);
-                        dlist.add(sb.toString());
-                    }
-                }
-            }
-            }
-            for(String i : dlist) set.remove(i);
+            stack.push(i);
         }
-        return list;
-        
+        System.out.print(max);
     }
+//      public static int largestRectangleArea(int[] heights) {
+//   Stack < Integer > stack = new Stack < > ();
+//   stack.push(-1);
+//   int length = heights.length;
+//   int maxArea = 0;
+//   for (int i = 0; i < length; i++) {
+//      while ((stack.peek() != -1) &&
+//       (heights[stack.peek()] >= heights[i])) {
+//       int currentHeight = heights[stack.pop()];
+//       int currentWidth = i - stack.peek() - 1;
+//       maxArea = Math.max(maxArea, currentHeight * currentWidth);
+//      }
+//      stack.push(i);
+//   }
+//   while (stack.peek() != -1) {
+//      int currentHeight = heights[stack.pop()];
+//      int currentWidth = length - stack.peek() - 1;
+//      maxArea = Math.max(maxArea, currentHeight * currentWidth);
+//   }
+//   return maxArea;
+//  }
 }
