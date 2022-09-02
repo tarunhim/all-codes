89bdf6fbda6c6ac591cec6b6e90603b534e3802b
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-02 13:28:59 +0530

    solve

--- random-java-codes/integer-to-english/Main.java
+++ random-java-codes/integer-to-english/Main.java
@@ -3,3 +3,23 @@ class Main{
         
     }
 }
+
+class Solution {
+    boolean flag = false;
+    public String numberToWords(int num) {
+        
+        if(flag == false && num == 0) return "Zero";
+        flag = true;
+        String[] ones = {"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten",
+    "Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen",
+                        "Nineteen"};
+        String[] tens = {"","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty",
+                        "Ninety"};
+if(num >= 1000000000) return (numberToWords(num/1000000000)+" Billion "+numberToWords(num%1000000000)).trim();
+if(num >= 1000000) return (numberToWords(num/1000000)+" Million "+numberToWords(num%1000000)).trim();
+if(num >= 1000) return (numberToWords(num/1000)+" Thousand "+numberToWords(num%1000)).trim();
+if(num >= 100) return (numberToWords(num/100)+" Hundred "+numberToWords(num%100)).trim();
+if(num >= 20) return (tens[num/10]+" "+numberToWords(num%10)).trim();
+return ones[num];
+    }
+}
