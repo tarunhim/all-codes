d5236e6adbd254ddfaa906894badd221bffb2f7b
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-05 18:15:22 +0530

    solve

--- random-java-codes/maximum-rows-covered-by-columns/Main.java
+++ random-java-codes/maximum-rows-covered-by-columns/Main.java
@@ -1,7 +1,10 @@
 class Main{
-    int[][] mat = {{0,0,0},{1,0,1},{0,1,1},{0,0,1}};
+    public static void main(String[] args) {
+        int[][] mat = {{0,0,0},{1,0,1},{0,1,1},{0,0,1}};
     int cols = 2;
+    // System.out.print(new Solution().maximumRows(mat,cols));
     
+    }
 }
 
 
