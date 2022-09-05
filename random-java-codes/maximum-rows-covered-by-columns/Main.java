07a893e053efc15bf7c9cf79438d60a569e21f1f
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-05 18:09:53 +0530

    solve

---
+++ random-java-codes/maximum-rows-covered-by-columns/Main.java
@@ -0,0 +1,27 @@
+class Solution{
+	public int maximumRows(int[][] mat, int cols){
+		return solve(mat,new boolean[mat[0].length],0,cols);
+	}
+	int solve(int[][] mat,boolean[] arr, int i, int cols){
+		if(i >= mat[0].length || cols <= 0) return 0;
+		arr[i] = true;
+		int count = 0;
+		if(cols == 1){
+			for(int[] row : mat){
+				boolean flag = true;
+				for(int j = 0; j < row.length; ++j){
+					if(arr[j]) continue;
+					if(row[j] == 1){
+						flag = false;
+						break;
+					}
+				}
+				if(flag){count++;}
+			}
+		}
+		int with = solve(mat,arr,i+1,cols-1);
+		arr[i] = false;
+		int without = solve(mat,arr,i+1,cols);
+		return Math.max(count,Math.max(with,without));
+	}
+}
