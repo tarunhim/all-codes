7d641e7744c2f54e9073d5f3929fbe105d4c7b1a
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-09 12:09:12 +0530

    done

--- Practice/Practice1.java
+++ Practice/Practice1.java
@@ -1,10 +1,16 @@
 import java.io.*; // for handling input/output
 import java.util.*; // contains Collections framework
 
-// don't change the name of this class
-// you can add inner classes if needed
-public class Practice1 {
-    public static void main(String[] args) {
-        System.out.println("this is a trial file");
-    }
+class Test {
+public static void swap(Integer i, Integer j) {
+     Integer temp = new Integer(i);
+     i = j;
+     j = temp;
+  }
+  public static void main(String[] args) {
+     Integer i = new Integer(10);
+     Integer j = new Integer(20);
+     swap(i, j);
+     System.out.println("i = " + i + ", j = " + j);
+  }
 }
