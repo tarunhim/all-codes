d303db0ae1ae7113ae88a81e61aea027fde14f46
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-09 20:02:06 +0530

    add

--- Practice/Practice1.java
+++ Practice/Practice1.java
@@ -1,16 +1,22 @@
 import java.io.*; // for handling input/output
 import java.util.*; // contains Collections framework
 
-class Practice1 {
-public static void swap(Integer i, Integer j) {
-     Integer temp = new Integer(i);
-     i = j;
-     j = temp;
-  }
-  public static void main(String[] args) {
-     Integer i = new Integer(10);
-     Integer j = new Integer(20);
-     swap(i, j);
-     System.out.println("i = " + i + ", j = " + j);
-  }
+class Practice1{
+	public static void main(String[] args){
+		int[] arr = {1,2,3,3,-4,4};
+		int k = 6;
+		int ans = solve(arr,0,k);
+		System.out.println(ans);
+	}
+	static int solve(int[] arr, int index, int k){
+		int ret = 0;
+		System.out.println(k);
+		if(k == 0) ret++;
+		if(index >= arr.length) return ret;
+		
+		int with = solve(arr,index+1,k-arr[index]);
+		int without = solve(arr, index+1,k);
+		
+		return ret+with+without;
+	}
 }
