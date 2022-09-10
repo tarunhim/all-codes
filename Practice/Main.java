d824063798b5c2880ffefb3d888f654ef2fd31ff
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-10 11:16:08 +0530

    done

--- Practice/Main.java
+++ Practice/Main.java
@@ -5,43 +5,8 @@ class Main
 {
 	public static void main(String[] args)
 	{	
-		AdderII ad = new AdderII();
-		int ans = ad.add(1,1);
-		System.out.print(ans);
+		
+		System.out.print("Tarun");
 	}
 }
 // ient you interface here
-interface Adder
-{
-    public int add();
-    public int add(int a, int b);
-    public int add(int a, int b, int c);
-}
-
-class AdderII implements Adder
-{
-    int num1;
-    int num2;
-    AdderII()
-    {
-        num1 = 0;
-        num2 = 0;
-    }
-    AdderII(int a, int b)
-    {
-        num1 = a;
-        num2 = b;
-    }
-    public int add(int a, int b, int c)
-    {
-        return num1*a+num2*b-c*c;
-    }
-    public int add()
-    {
-        return num1+num2;
-    }
-    public int add(int a, int b)
-    {
-        return (a*a)+(b*b);
-    }
-}
