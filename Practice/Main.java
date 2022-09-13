90e5c8b8f8e93e9b329f9cec7831b0142b34e72b
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-13 20:44:14 +0530

    dont know

--- Practice/Main.java
+++ Practice/Main.java
@@ -2,6 +2,52 @@ class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.print("my name is tarun kumar chourasiya");
+		Circle circle = new Circle(2.1);
+		if(circle.getRadius() == 2.1)
+		System.out.println("Correct");
+		else
+		System.out.println("Wrong");
 	}
+}
+/* 
+     your code goes here
+    example:
+          class Circle{
+          
+         }
+*/
+class Circle
+{
+      double radius;
+      Circle(double radius)
+      {
+            this.radius = radius < 0 ? 0 : radius;
+      }
+      public double getRadius()
+      {
+            return this.radius;
+      }
+      public double getArea()
+      {
+            return this.radius*this.radius*3.14159265358979323846;
+      }
+}
+class Cylinder extends Circle
+{
+      double height;
+      Cylinder(double height, double radius)
+      {
+            super(radius);
+            System.out.println("Cylinder constructer :"+height+" "+radius);
+            this.height = height < 0 ? 0 : height;
+      }
+      public double getHeight()
+      {
+            return this.height;
+      }
+      public double getVolume()
+      {     
+            System.out.println("volumn: "+getArea()*this.height);
+            return getArea()*this.height;
+      }
 }
