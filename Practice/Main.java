6310a6edaf83f0439b0cc7226d4feb2a585bbe49
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-14 16:02:11 +0530

    new question

--- Practice/Main.java
+++ Practice/Main.java
@@ -2,52 +2,23 @@ class Main
 {
 	public static void main(String[] args)
 	{
-		Circle circle = new Circle(2.1);
-		if(circle.getRadius() == 2.1)
-		System.out.println("Correct");
-		else
-		System.out.println("Wrong");
+		int[] arr = {10, 30, 5, 60};
+		System.out.print(solve(arr,0,arr.length-1));
+		
 	}
-}
-/* 
-     your code goes here
-    example:
-          class Circle{
-          
-         }
-*/
-class Circle
-{
-      double radius;
-      Circle(double radius)
-      {
-            this.radius = radius < 0 ? 0 : radius;
-      }
-      public double getRadius()
-      {
-            return this.radius;
-      }
-      public double getArea()
-      {
-            return this.radius*this.radius*3.14159265358979323846;
-      }
-}
-class Cylinder extends Circle
-{
-      double height;
-      Cylinder(double height, double radius)
-      {
-            super(radius);
-            System.out.println("Cylinder constructer :"+height+" "+radius);
-            this.height = height < 0 ? 0 : height;
-      }
-      public double getHeight()
-      {
-            return this.height;
-      }
-      public double getVolume()
-      {     
-            System.out.println("volumn: "+getArea()*this.height);
-            return getArea()*this.height;
-      }
+	public static int solve(int[] arr, int i, int j)
+	{	
+		if(j <= i+1) return 0;
+
+		int min = Integer.MAX_VALUE;
+		
+		for(int k = i+1; k < j; ++k)
+		{
+			int count = solve(arr,i,k);
+			count += solve(arr,k,j);
+			count += arr[i]*arr[k]*arr[j];
+			min = Math.min(min,count);
+		}
+		return min;
+	}
 }
