f1455c162cba2ae3875faf6a23d3cd026c821b6e
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-16 21:33:52 +0530

    solve

--- Practice/Main.java
+++ Practice/Main.java
@@ -6,20 +6,4 @@ class Main
 		System.out.print(solve(arr,0,arr.length-1));
 		
 	}
-	public static int solve(int[] arr, int i, int j)
-	{	
-		if(j <= i+1) return 0;
-
-		int min = Integer.MAX_VALUE;
-		
-		for(int k = i+1; k < j; ++k)
-		{
-			int count = solve(arr,i,k);
-			count += solve(arr,k,j);
-			count += arr[i]*arr[k]*arr[j];
-
-			min = Math.min(min,count);
-		}
-		return min;
-	}
 }
