8e1fd884116ef1ccae3bc14b0f17412f206ebf83
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-18 12:03:25 +0530

    solve

--- Practice/Main.java
+++ Practice/Main.java
@@ -1,9 +1,8 @@
-class Main
-{
-	public static void main(String[] args)
-	{
-		int[] arr = {10, 30, 5, 60};
-		System.out.print(solve(arr,0,arr.length-1));
-		
-	}
+
+public class Main {
+public Main(int i) {
+}
+}
+
+class B extends Main {
 }
