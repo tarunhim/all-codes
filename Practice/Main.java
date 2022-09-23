1f61b07a18d6c21b9ff2d366530191418512c1a2
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-23 18:07:31 +0530

    done

--- Practice/Main.java
+++ Practice/Main.java
@@ -5,7 +5,7 @@ public class Main
 
 	public static void main(String[] args) 
 	{
-		Integer[] arr = {4,2,5,2,3,6,1};
+		int[] arr = {4,2,5,2,3,6,1};
 		Arrays.sort(arr,(a,b) -> a-b);
 		System.out.println(Arrays.toString(arr)); 
 	}
