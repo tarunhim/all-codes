b31a61bc1937d1dd02bf1dcd35460f3598fe8df4
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-23 16:07:58 +0530

    done

--- Practice/Main.java
+++ Practice/Main.java
@@ -6,7 +6,12 @@ public class Main
 	public static void main(String[] args) 
 	{
 		int[] arr = {4,2,5,2,3,6,1};
-		Arrays.sort(arr);
+		Arrays.sort(arr,new Comparator<int>(){
+			public int compare(int a, int b)
+			{
+				return a-b;
+			}
+		});
 		System.out.println(Arrays.toString(arr)); 
 	}
 }
