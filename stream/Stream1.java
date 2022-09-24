81ccc22d6455175b1a199cfe1e92470e4b6ae0a7
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-24 12:38:04 +0530

    done

--- stream/Stream1.java
+++ stream/Stream1.java
@@ -2,9 +2,7 @@ class Stream1
 {
 	public static void main(String[] args)
 	{
-		Pair<String,Integer> p = new Pair<>("tarun",9384);
-		System.out.println(p.name);
-		System.out.println(p.id);
+		
 	}
 }
 
