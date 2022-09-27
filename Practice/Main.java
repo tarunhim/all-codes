96de01b4d514af1d3f118891105f94865391ae64
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-27 11:23:16 +0530

    solve

--- Practice/Main.java
+++ Practice/Main.java
@@ -3,6 +3,11 @@ class Solution
     
 	public int lengthOfLIS(int[] nums, int k) 
 	{
-        	
+        	int[] arr = new int[5];
+		for(int i = 5; i > 0; i-)
+		arr[5-i] = i;
+		
+		for(int i = 0; i < 5; i++)
+		System.out.print(arr[i]);
 	}
 }
