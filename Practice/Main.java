60dae5bd76507282237510ffc2d986d7b23d6e41
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-27 11:25:26 +0530

    solve

--- Practice/Main.java
+++ Practice/Main.java
@@ -1,12 +1,12 @@
-class Solution 
+class Main
 {
     
-	public int lengthOfLIS(int[] nums, int k) 
+	public static void main(String[] args) 
 	{
         	int[] arr = new int[5];
-		for(int i = 5; i > 0; i-)
+		for(int i = 5; i > 0; i--)
 		arr[5-i] = i;
-		
+		Arrays.fill(arr,1,4,8);
 		for(int i = 0; i < 5; i++)
 		System.out.print(arr[i]);
 	}
