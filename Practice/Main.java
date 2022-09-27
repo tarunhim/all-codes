c418a156a06270d413ff10538dc303a18c184430
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-27 11:27:20 +0530

    solve

--- Practice/Main.java
+++ Practice/Main.java
@@ -7,7 +7,7 @@ class Main
         	int[] arr = new int[5];
 		for(int i = 5; i > 0; i--)
 		arr[5-i] = i;
-		Arrays.fill(arr,1,5,8);
+		Arrays.fill(arr,1,1,8);
 		for(int i = 0; i < 5; i++)
 		System.out.print(arr[i]);
 	}
