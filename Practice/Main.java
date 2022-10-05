d82ae2d7bafd19dd8afb3134f302d0cd8031cbbc
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-10-05 19:28:05 +0530

    solve

--- Practice/Main.java
+++ Practice/Main.java
@@ -4,13 +4,7 @@ class Main
     
 	public static void main(String[] args) 
 	{
-        	int[] arr = new int[5];
-		for(int i = 5; i > 0; i--)
-		arr[5-i] = i;
-		Arrays.fill(arr,1,1,8);
-		for(int i = 0; i < 5; i++)
-		System.out.print(arr[i]);
-
+        	System.out.println("tarun");
 
 	}
 }
