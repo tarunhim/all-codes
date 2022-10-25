3adaaa40f5c89b5c19428180c2be035e231bd9ac
tarun kumar chourasiya <tkcthousand@gmail.com>
2022-10-25 15:55:06 +0530

    done

--- Practice/Main.java
+++ Practice/Main.java
@@ -1,11 +1,7 @@
-import java.util.*;
 class Main
 {
-    
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-        	System.out.println("tarun");
-	
-
+		System.out.println("Tarun kumar chourasiya");
 	}
 }
