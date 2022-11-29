a4a420954b20d7f4741ed52b68430b6389b7418a
tarun kumar chourasiya <tkcthousand@gmail.com>
2022-11-29 11:09:11 +0530

    coding

--- Practice/Main.java
+++ Practice/Main.java
@@ -1,4 +1,28 @@
-
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+        boolean shouldContinue = true;
+        while (shouldContinue) {
+            shouldContinue = false;
+            ListNode dummyHead = new ListNode();
+            ListNode pointer = dummyHead;
+            ListNode node = head;
+            while (node != null) {
+                if (node.next != null && node.val < node.next.val) {
+                    shouldContinue = true;
+                } else {
+                    pointer.next = node;
+                    pointer = node;
+                    // System.out.println(pointer.val);
+                }
+                node = node.next;
+            }
+            pointer.next = null;
+            head = dummyHead.next;
+            // System.out.println("-----------------");
+        }
+        return head;
+    }
+}
 class Main
 {
 	public static void main(String[] args)
