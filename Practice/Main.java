97f2047ba35af07a9228db7d2cdb0c4de72c5255
tarunkumarchourasiya <tkcthousand@gmail.com>
2023-07-29 13:07:06 +0530

    some changes from personal computer

--- Practice/Main.java
+++ Practice/Main.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
        for(int i = 0; i < 10 ; ++i) { 
-	       System.out.println("print"+i);
+	       System.out.println("print"+i+"tarun");
        }
     }
 }
